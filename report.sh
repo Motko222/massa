0efec5d0d65da555990a35f6b7ad585740ecae2a
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-09-11 22:30:40 +0200

    Create report.sh

---
+++ report.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+ver=$(cat ~/logs/massa.log | grep -a "Node version :" | tail -1 | awk '{print $8}')
+pid=$(pgrep massa-node)
+note=""
+network="testnet"
+type="-"
+now=$(date +'%Y-%m-%d %H:%M:%S')
+foldersize=$(du -hs ~/massa | awk '{print $1}')
+logsize=$(du -hs ~/logs/massa.log | awk '{print $1}')
+
+
+if [ -z $pid ];
+then status="error";
+ note="node restarted";
+ cd ~/massa/massa-node;
+ cargo run --release -- -p $massapwd >~/logs/massa.log 2>&1 &
+else status="ok";
+fi
+
+#echo "updated:            "$now
+#echo "version:            "$ver
+#echo "process:            "$pid
+#echo "status:             "$status
+#echo "note:               "$note
+#echo "network:            "$network
+#echo "type:               "$type
+#echo "folder size:        "$foldersize
+#echo "log size:           "$logsize
+#echo "id:                 "$massapwd
+#echo "wallet:             "$massaadr
+
+echo "updated='$now'"
+echo "version='$ver'"
+echo "process='$pid'"
+echo "status="$status
+echo "note='$note'"
+echo "network='$network'"
+echo "type="$type
+echo "folder=$foldersize"
+echo "log=$logsize" 
+echo "id=$massapwd" 
+echo "wallet=$massaadr"
