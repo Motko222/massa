847185809b08f3badbc6f3f772f84e55fef0d0fa
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-09-16 23:53:11 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,6 +15,14 @@ case $sure in
   rustup default 1.72.0
   rustc --version
   git clone --branch testnet https://github.com/massalabs/massa.git
+
+  if [ -f ~/scripts/massa/config/env] 
+    then
+      echo "Config file found."
+    else
+      echo "Config file not found, creating one."
+      cp ~/scripts/massa/config/env.sample ~/scripts/massa/config/env
+  fi
  ;;
 *) echo Cancelled... ;;
 esac
