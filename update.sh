5885071055912710ddf6b2ecebc71506b9d4e688
Motko222 <77466238+Motko222@users.noreply.github.com>
2023-09-16 23:57:27 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,9 +6,9 @@ case $sure in
   echo "Killing process..."
   pkill massa-node
   echo "Backing up..."
-  cp ~/massa/massa-node/config/node_privkey.key ~/backup/massa/
-  cp ~/massa/massa-node/config/config.toml ~/backup/massa/
-  cp ~/massa/massa-client/wallet.dat ~/backup/massa/
+  cp ~/massa/massa-node/config/node_privkey.key ~/scripts/massa/config
+  cp ~/massa/massa-node/config/config.toml ~/scripts/massa/config
+  cp ~/massa/massa-client/wallet.dat ~/scripts/massa/config
   echo "Updating..."
   cd ~/massa
   git stash
@@ -16,9 +16,9 @@ case $sure in
   git checkout testnet
   git pull
   echo "Restoring..."
-  cp ~/backup/massa//node_privkey.key ~/massa/massa-node/config
-  cp ~/backup/massa//config.toml ~/massa/massa-node/config
-  cp ~/backup/massa//wallet.dat  ~/massa/massa-client
+  cp ~/scripts/massa/config/node_privkey.key ~/massa/massa-node/config
+  cp ~/scripts/massa/config/config.toml ~/massa/massa-node/config
+  cp ~/scripts/massa/config/wallet.dat  ~/massa/massa-client
   echo "Done..."
  ;;
 *) echo "Cancelled..." ;;
