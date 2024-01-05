2e8bcff1d628106aaca6a96e621f20d82dbd2d09
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-01-05 23:16:18 +0100

    Update client.sh

--- client.sh
+++ client.sh
@@ -6,6 +6,7 @@ echo "--- COMMANDS ---"
 echo "wallet_info"
 echo "buy_rolls "$massaadr" 1 0"
 echo "node_start_staking "$massaadr
+echo "wallet_add_secret_keys <your_secret_key>"
 #echo "node_testnet_rewards_program_ownership_proof "$massaadr" "$discordid
 echo "---"
 cd ~/massa/massa-client
