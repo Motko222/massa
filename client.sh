565d4567caa2a8e1c97e5d5e1a10aec20aea6888
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-01-05 23:15:06 +0100

    Update client.sh

--- client.sh
+++ client.sh
@@ -6,7 +6,7 @@ echo "--- COMMANDS ---"
 echo "wallet_info"
 echo "buy_rolls "$massaadr" 1 0"
 echo "node_start_staking "$massaadr
-echo "node_testnet_rewards_program_ownership_proof "$massaadr" "$discordid
+#echo "node_testnet_rewards_program_ownership_proof "$massaadr" "$discordid
 echo "---"
 cd ~/massa/massa-client
 cargo run -- --wallet wallet.dat
