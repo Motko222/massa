55cd8ab1bb3e4dc6376f7a76458c19676d3296c9
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-01-13 16:16:32 +0100

    Update report.sh

--- report.sh
+++ report.sh
@@ -3,31 +3,34 @@
 source ~/scripts/massa/config/env
 
 #ver=$(cat ~/logs/massa.log | grep -a "Node version :" | tail -1 | awk '{print $8}')
-cat ~/massa/massa-node/Cargo.toml | grep "version =" | cut -d \" -f 2
+ver=$(cat ~/massa/massa-node/Cargo.toml | grep "version =" | cut -d \" -f 2)
+service=$(sudo systemctl status massad --no-pager | grep "active (running)" | wc -l)
 pid=$(pgrep /root/massa/target/release/massa-node)
 note=""
 network="mainnet"
 type="node"
 now=$(date +'%y-%m-%d %H:%M')
 foldersize=$(du -hs ~/massa | awk '{print $1}')
-logsize=$(du -hs ~/logs/massa.log | awk '{print $1}')
+cpu=$(sudo systemctl status massad --no-pager | grep CPU | awk '{print $2}')
+mem=$(sudo systemctl status massad --no-pager | grep memory | awk '{print $2}')
+#logsize=$(du -hs ~/logs/massa.log | awk '{print $1}')
 
-
-if [ -z $pid ];
+if [ $service -ne 1 ]
 then 
   status="error";
-  note="node not running"
-# note="node restarted";
-# cd ~/massa/massa-node;
-# cargo run --release -- -p $massapwd >~/logs/massa.log 2>&1 &
-
+  note="service not running"
 else 
   status="ok";
 fi
 
+#legacy launcher
+# note="node restarted";
+# cd ~/massa/massa-node;
+# cargo run --release -- -p $massapwd >~/logs/massa.log 2>&1 &
+
 echo "updated='$now'"
 echo "version='$ver'"
-echo "process='$pid'"
+echo "service=$service"
 echo "status="$status
 echo "note='$note'"
 echo "network='$network'"
@@ -36,3 +39,5 @@ echo "folder=$foldersize"
 #echo "log=$logsize" 
 echo "id=$massapwd" 
 echo "wallet=$massaadr"
+echo "cpu=$cpu"
+echo "memory=$memory"
