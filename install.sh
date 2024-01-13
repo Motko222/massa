b9ebf1c32f11133860b041c6170835ce618906cb
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-01-13 15:29:38 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,9 +20,29 @@ case $sure in
     then
       echo "Config file found."
     else
-      echo "Config file not found, creating one."
-      cp ~/scripts/massa/config/env.sample ~/scripts/massa/config/env
+     read -p "Password? " massapwd
+     read -p "Wallet? " massaadr
+     echo "massapwd="$massapwd >> ~/scripts/massa/config/env
+     echo "massaadr="$massaadr > ~/scripts/massa/config/env
+     echo "Config file created."
   fi
+
+printf "[Unit]
+Description=Massa Node
+After=network-online.target
+[Service]
+User=$USER
+WorkingDirectory=$HOME/massa/massa-node
+ExecStart=$HOME/massa/massa-node/massa-node -p $massapwd
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/massad.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable massad
+
  ;;
 *) echo Cancelled... ;;
 esac
