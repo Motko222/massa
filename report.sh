3c05a7de4404553e1d1620ca62dd0f63386f1289
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-01-13 16:02:15 +0100

    Update report.sh

--- report.sh
+++ report.sh
@@ -3,22 +3,27 @@
 source ~/scripts/massa/config/env
 
 #ver=$(cat ~/logs/massa.log | grep -a "Node version :" | tail -1 | awk '{print $8}')
+cat ~/massa/massa-node/Cargo.toml | grep "version =" | cut -d \" -f 2
 pid=$(pgrep /root/massa/target/release/massa-node)
 note=""
 network="mainnet"
-type="-"
+type="node"
 now=$(date +'%y-%m-%d %H:%M')
 foldersize=$(du -hs ~/massa | awk '{print $1}')
 logsize=$(du -hs ~/logs/massa.log | awk '{print $1}')
 
 
-#if [ -z $pid ];
-#then status="error";
+if [ -z $pid ];
+then 
+  status="error";
+  note="node not running"
 # note="node restarted";
 # cd ~/massa/massa-node;
 # cargo run --release -- -p $massapwd >~/logs/massa.log 2>&1 &
-#else status="ok";
-#fi
+
+else 
+  status="ok";
+fi
 
 echo "updated='$now'"
 echo "version='$ver'"
@@ -28,6 +33,6 @@ echo "note='$note'"
 echo "network='$network'"
 echo "type="$type
 echo "folder=$foldersize"
-echo "log=$logsize" 
+#echo "log=$logsize" 
 echo "id=$massapwd" 
 echo "wallet=$massaadr"
