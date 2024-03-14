56c8cedbd28f37b61e15696c6f5aecb069974656
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-03-14 10:05:36 +0100

    Update client.sh

--- client.sh
+++ client.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-source ~/scripts/massa/config/env
+source ~/.bash_profile
 
 echo "--- COMMANDS ---"
 echo "wallet_info"
-echo "buy_rolls "$massaadr" 1 0"
-echo "node_start_staking "$massaadr
+echo "buy_rolls "$MASSA_WALLET" 1 0"
+echo "node_start_staking "$MASSA_WALLET
 echo "wallet_add_secret_keys <your_secret_key>"
 #echo "node_testnet_rewards_program_ownership_proof "$massaadr" "$discordid
 echo "---"
 cd ~/massa/massa-client
-cargo run --release -- -p $massapwd
+cargo run --release -- -p $MASSA_PWD
 #cargo run -- --wallet wallet.dat
