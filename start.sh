437059e5b93ac3a0381cef11991e36ca348fbfd4
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-03-14 10:10:38 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-source ~/scripts/massa/config/env
+source ~/.bash_profile
 
 #echo "Killing process..."
 #pkill massa-client
 #sleep 1s
 #cd ~/massa/massa-node
 #echo "Starting..."
-#cargo run --release -- -p $massapwd >~/logs/massa.log 2>&1 &
+#cargo run --release -- -p $MASSA_PWD >~/logs/massa.log 2>&1 &
 
 sudo systemctl restart massad
 sudo journalctl -u massad.service -f --no-hostname -o cat
