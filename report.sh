9820866ad31aabf5fd15a32252b4c49cff4630ad
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-03-14 10:03:26 +0100

    Update report.sh

--- report.sh
+++ report.sh
@@ -1,43 +1,44 @@
 #!/bin/bash
 
-source ~/scripts/massa/config/env
-
-#ver=$(cat ~/logs/massa.log | grep -a "Node version :" | tail -1 | awk '{print $8}')
-ver=$(cat ~/massa/massa-node/Cargo.toml | grep "version =" | cut -d \" -f 2)
+version=$(cat ~/massa/massa-node/Cargo.toml | grep "version =" | cut -d \" -f 2)
 service=$(sudo systemctl status massad --no-pager | grep "active (running)" | wc -l)
-pid=$(pgrep /root/massa/target/release/massa-node)
-note=""
-network="mainnet"
-type="node"
-now=$(date +'%y-%m-%d %H:%M')
+chain="mainnet"
 foldersize=$(du -hs ~/massa | awk '{print $1}')
 cpu=$(sudo systemctl status massad --no-pager | grep CPU | awk '{print $2}')
 mem=$(sudo systemctl status massad --no-pager | grep Memory | awk '{print $2}')
-#logsize=$(du -hs ~/logs/massa.log | awk '{print $1}')
+id=massa-$MASSA_ID
+bucket=node
 
 if [ $service -ne 1 ]
 then 
   status="error";
-  note="service not running"
+  message="service not running"
 else 
   status="ok";
 fi
 
-#legacy launcher
-# note="node restarted";
-# cd ~/massa/massa-node;
-# cargo run --release -- -p $massapwd >~/logs/massa.log 2>&1 &
+cat << EOF
+{
+  "id":"$id",
+  "machine":"$MACHINE",
+  "chain":"$chain",
+  "type":"node",
+  "status":"$status",
+  "message":"$message",
+  "service":$service,
+  "updated":"$(date --utc +%FT%TZ)"
+}
+EOF
 
-echo "updated='$now'"
-echo "version='$ver'"
-echo "service=$service"
-echo "status="$status
-echo "note='$note'"
-echo "network='$network'"
-echo "type="$type
-echo "folder=$foldersize"
-#echo "log=$logsize" 
-echo "id=$massapwd" 
-echo "wallet=$massaadr"
-echo "cpu=$cpu"
-echo "memory=$memory"
+# send data to influxdb
+if [ ! -z $INFLUX_HOST ]
+then
+ curl --request POST \
+ "$INFLUX_HOST/api/v2/write?org=$INFLUX_ORG&bucket=$bucket&precision=ns" \
+  --header "Authorization: Token $INFLUX_TOKEN" \
+  --header "Content-Type: text/plain; charset=utf-8" \
+  --header "Accept: application/json" \
+  --data-binary "
+    status,node=$id,machine=$MACHINE status=\"$status\",message=\"$message\",version=\"$version\",url=\"$url\",chain=\"$chain\" $(date +%s%N) 
+    "
+fi
