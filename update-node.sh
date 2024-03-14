960185b287b3d13ea531ec6921dec222458f52bc
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-03-14 10:15:35 +0100

    Update update-node.sh

--- update-node.sh
+++ update-node.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 read -p "Sure ? " sure
-case $sure in
- y|Y)
-  echo "Killing process..."
-  pkill massa-node
-  echo "Backing up..."
-  cp ~/massa/massa-node/config/node_privkey.key ~/scripts/massa/config
-  cp ~/massa/massa-node/config/config.toml ~/scripts/massa/config
-  cp ~/massa/massa-client/wallet.dat ~/scripts/massa/config
-  echo "Updating..."
-  cd ~/massa
-  git stash
-  git remote set-url origin https://github.com/massalabs/massa.git
-  git checkout mainnet
-  git pull
-  echo "Restoring..."
-  cp ~/scripts/massa/config/node_privkey.key ~/massa/massa-node/config
-  cp ~/scripts/massa/config/config.toml ~/massa/massa-node/config
-  cp ~/scripts/massa/config/wallet.dat  ~/massa/massa-client
-  echo "Done..."
- ;;
-*) echo "Cancelled..." ;;
-esac
+if [ $sure -ne "y" -a $sure -ne "Y" ]; then exit 1; fi
+
+echo "Backing up..."
+cp ~/massa/massa-node/config/node_privkey.key ~/
+cp ~/massa/massa-node/config/config.toml ~/
+cp ~/massa/massa-client/wallet.dat ~/
+
+echo "Updating..."
+cd ~/massa
+git stash
+git remote set-url origin https://github.com/massalabs/massa.git
+git checkout mainnet
+git pull
+
+echo "Restoring..."
+cp ~/node_privkey.key ~/massa/massa-node/config
+cp ~/config.toml ~/massa/massa-node/config
+cp ~/wallet.dat ~/massa/massa-client
+
+echo "Done..."
+
+ 
