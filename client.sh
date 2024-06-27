62fe74f379429d2f9f85b4237c266200ded3fb68
Motko <77466238+Motko222@users.noreply.github.com>
2024-06-27 09:47:16 +0200

    Update client.sh

--- client.sh
+++ client.sh
@@ -4,7 +4,7 @@ source ~/.bash_profile
 
 echo "--- COMMANDS ---"
 echo "wallet_info"
-echo "buy_rolls "$MASSA_WALLET" 1 0"
+echo "buy_rolls "$MASSA_WALLET" 1 0.01"
 echo "node_start_staking "$MASSA_WALLET
 echo "wallet_add_secret_keys <your_secret_key>"
 echo "---"
