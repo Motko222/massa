62034aeed952383c39e43f58a31d407b920c2e23
Motko <77466238+Motko222@users.noreply.github.com>
2025-01-23 21:15:25 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,19 +21,3 @@ rustup default 1.74.1
 rustc --version
 git clone --branch mainnet https://github.com/massalabs/massa.git
 
-printf "[Unit]
-Description=Massa Node
-After=network-online.target
-[Service]
-User=root
-WorkingDirectory=/root/massa/massa-node
-ExecStart=/root/.cargo/bin/cargo run --release -- -p $PASSWORD
-Restart=on-failure
-RestartSec=3
-LimitNOFILE=65535
-[Install]
-WantedBy=multi-user.target" > /etc/systemd/system/massad.service
-
-sudo systemctl daemon-reload
-sudo systemctl enable $folder
-
