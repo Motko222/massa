9ae90a693ddc4217d28a64b12ad94f7e239ef127
Motko <77466238+Motko222@users.noreply.github.com>
2025-01-23 21:10:30 +0100

    Update update-scripts.sh

--- update-scripts.sh
+++ update-scripts.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-cd ~/scripts/massa
+path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
+folder=$(echo $path | awk -F/ '{print $NF}')
+
+cd $path
 git stash push --include-untracked
 git pull
 chmod +x *.sh
