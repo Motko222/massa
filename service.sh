a2a07abe69e261432d6cf8472099d0699f95ac4e
Motko <77466238+Motko222@users.noreply.github.com>
2025-01-23 21:13:06 +0100

    Create service.sh

---
+++ service.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
+folder=$(echo $path | awk -F/ '{print $NF}')
+cd $path
+source config
+
+printf "[Unit]
+Description=Massa Node
+After=network-online.target
+[Service]
+User=root
+WorkingDirectory=/root/massa/massa-node
+ExecStart=/root/.cargo/bin/cargo run --release -- -p $PASSWORD
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/massad.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable $folder
