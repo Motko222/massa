d9e07e0236875d4abed20763dd80005c330be4da
Motko <77466238+Motko222@users.noreply.github.com>
2025-01-23 21:14:28 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ if [ $sure -ne "y" -a $sure -ne "Y" ]; then exit 1; fi
 
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
+cd $path
+source config
 
 cd ~
 sudo systemctl stop $folder 
