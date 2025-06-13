5d481242ac4fb1b9ce1cff52d2e8861ca78a802f
Motko <77466238+Motko222@users.noreply.github.com>
2025-06-13 09:49:59 +0200

    Update update-node.sh

--- update-node.sh
+++ update-node.sh
@@ -4,9 +4,10 @@ read -p "Sure ? " sure
 if [ $sure -ne "y" -a $sure -ne "Y" ]; then exit 1; fi
 
 echo "Backing up..."
-cp ~/massa/massa-node/config/node_privkey.key ~/
-cp ~/massa/massa-node/config/config.toml ~/
-cp ~/massa/massa-client/wallet.dat ~/
+cp -r ~/massa/massa-node/config/staking_wallets ~/backup/massa/massa-node/config
+cp -r ~/massa/massa-client/wallets ~/backup/massa/massa-client
+#cp -r ~/massa/massa-node/config/config.toml ~/backup/massa/massa-node/config
+#cp -r ~/massa/massa-client/config/config.toml ~/backup/massa/massa-client/config
 
 read -p "tag? (https://github.com/massalabs/massa/releases) " tag
 
@@ -21,9 +22,10 @@ git fetch
 git checkout $tag
 
 echo "Restoring..."
-cp ~/node_privkey.key ~/massa/massa-node/config
-cp ~/config.toml ~/massa/massa-node/config
-cp ~/wallet.dat ~/massa/massa-client
+cp -r ~/backup/massa/massa-node/config/staking_wallets  ~/massa/massa-node/config
+cp -r ~/backup/massa/massa-client/wallets ~/massa/massa-client
+#cp ~/backup/massa/massa-node/config/config.toml ~/massa/massa-node/config
+#cp ~/backup/massa/massa-client/config/config.toml ~/massa/massa-client/config
 
 echo "Done..."
 
