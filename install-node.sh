aa54c5c2ba6ebd4d9184cb7b9b9e38a7690b2869
Motko <77466238+Motko222@users.noreply.github.com>
2025-10-23 08:24:09 +0200

    Update and rename install.sh to install-node.sh

--- install-node.sh
+++ install-node.sh
@@ -12,12 +12,5 @@ cd ~
 sudo systemctl stop $folder 
 
 rm -r ~/massa
-sudo apt install pkg-config curl git build-essential libssl-dev libclang-dev cmake
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-source ~/.cargo/env
-rustc --version
-rustup toolchain install 1.74.1
-rustup default 1.74.1
-rustc --version
 git clone --branch mainnet https://github.com/massalabs/massa.git
 
